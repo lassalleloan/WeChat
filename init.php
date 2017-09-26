9b8455d81aac0a47d20e8dc8737e7ba78785438f
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-09-26 09:25:22 +0200

    Change digest

--- init.php
+++ init.php
@@ -33,7 +33,7 @@
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         username VARCHAR(255) UNIQUE NOT NULL,
                         salt VARCHAR(255) NOT NULL,
-                        password VARCHAR(88) NOT NULL,
+                        digest VARCHAR(88) NOT NULL,
                         active INTEGER NOT NULL,
                         role INTEGER NOT NULL,
                         FOREIGN KEY(role) REFERENCES roles(id));");
@@ -63,27 +63,27 @@
         $users = array(
                     array('username' => 'root',
                         'salt' => 'OYiKElgWm0U6q6aVnnA0pWYYIdvSklFV4h0G3VqTg5bXoRc5S514CMVPLAIBkkcz5K5aqEOmp3YMI4vzuJVv7zY0NSkoHvdwt6aAHJutrNv8tVhudqzlOHXHTxB66ZcmuK2BHnFMsuipZQHmyMlDSga0hU2feRJiO6xxLkKd1twXNrdopam8aL6vqjtSpML69Er2U8IZRGuE6Ng1MQS1KdTBefXwV0iijYvEwWNWv2HNUb68qcYRf0zqoPXfl7l',
-                        'password' => 'kL88aBYpfOaaEHHFX/sqwKWNwuWmIIkfv+nyopm2f3oE652GEBNK3JCipjladpdtrSEFDPPxvAijJ7C1DXoDzw==', //toor
+                        'digest' => 'Ghq9k4oSb6mHqrZvV8FnqXIYLGFAr7POLJ3kiRGcoQ5Fc/dQLYnCxMOYp65Lx9Z7l9d6zJanNiun0MTlAzgrig==', //base64(SHA512(username + salt + toor))
                         'active' => '1',
                         'role' => '1'),
                     array('username' => 'toor',
                         'salt' => 'O7Bj8OS6u9aGHTbPQ4S9RlGD56sVdSIipwaNlpSAeabM3fw85HnHdNXaAr8o4BscV0B5TB2fyENrkX6sIIsWJQ6A5wLjSet8lhm9okeRYapn4bwHitF1mlXwZbiThd8AaurWZPDNksqumreWQJBhXZR7G4V9lCnU0QX0FInv95M6m580pM53BqWIS03DCMT4QHcLe8yIpaR0dmnhTsiCxoytdfHWXr6zUAoyWo04osiYEIMCI9so15BOmNE2e9u',
-                        'password' => '3sMmC3FwMY9FUGUpLQeiat/wbx97+Ki0CHfwwlFZjpjPetim/iQAijqEe3UmCXODfXZoZ27AAjX4ZidGKB2L5g==', //root
+                        'digest' => 'iwKbLW3PXsgaN/kWlMAe5AL9rceA6um4C3S6Kizpi7eflJNkokBti5w0W4WT2YUf3SOORfyDG+PuP8e2rddmGA==', //base64(SHA512(username + salt + root))
                         'active' => '0',
                         'role' => '1'),
                     array('username' => 'loan',
                         'salt' => 'ERmaWoe63V8ao5mn1YuvlT5aTw9BRbBksRBsxGS3ypIa9dKLDcxwWb9Rg7q4Vl2BkWMZMPfLoKCwOzFrdaGyXa3irdSosU5WuPL9fU49dIhfu0xwX2VkuPbhSoknkkuay0ingNMqvr1X0c38crosAh8RkyJKNurgmqu5yNpunzwaT8MDiEM806gKvOqEeGT7KET0uneKLFSYBqPvHWcgf8bMw9rs5ZdZi5X4U0r4Rhvi68NRPW6bTz1cB9qpGG6',
-                        'password' => 'sxJdFk8z/5Hz2D/V6dCHD7t1a80d1pvP/L6JZ+dlC/QpaWMUINFKp7iA7mBjMMHIgNAw6gzA/z7NUn/ZaT+eBA==', //123
+                        'digest' => 'KCq2TlP2fCvYJMa6VARoJv0nB6JTZSKJfHHqBoNBFKIZy9A+aqlcXNZXqoEQLaCXwHyc7MW95BJP8uFCtp6Z6w==', //base64(SHA512(username + salt + 123))
                         'active' => '1',
                         'role' => '2'),
                     array('username' => 'wojciech',
                         'salt' => 'HqW9Jvu7rvK0iL8KUiuHrj8KuyNpBpQO5TwTmkATc1pSnRdq7f9WQEPxx0J2wxhULlGMbZBLL1SzL89aPlCozDbOH04dZUqigZUGJScKblDhhSdFOrgsO8aCs1GgoRuVofZQeg1lCFi4yEHeHPf3DBfJGtWvpfZAFkyaCW1JJq55zcnv95ctIIMqrUkhNcJpdiso94k4MWbUDZrt13LDTPicxKmPIS82y1hQdbMNdiKGW4hGg0Hn86Pgk8yyTog',
-                        'password' => 'WYo6Wi7iyOWrD7sm8MhuHLlucJwI3oR7B07QG/yZg3BDsc//jxP6fe/UiW5gmQ9QMvpfUiYcPOlqdTnSirHrEA==', //456
+                        'digest' => 'ZPUCEF+S3ORBU/9+LP+cqYLxd7U1IyXvVxuC+hO4eV2V46svARmaVYPr26yDYG2W8GgDaEuO9fyJuic+jXQFKg==', //base64(SHA512(username + salt + 456))
                         'active' => '1',
                         'role' => '2'),
                     array('username' => 'tano',
                         'salt' => 'beIbenqv8SYx6lQGwm7VGIXSuJa86rGKke2fDkYdovQhQeLEOdWSNcyrXxdjFlDiyF5g0ZNUh2kXNefiYnK1i4QvDgZu9K3ELEf5atro9EkNN9r4HhMVNh9SwXKFN9voQIWO6RJUY9tRcMucGfUwPBDorfCM5ewd3AtSvMAilhxy4dQPOl8dAYbasCeMcUNlTSUaVqIPdKntbUTpxmFycSOcpSeGFDuuMyLKRFtxnmKawNk1BghlSWv0eySDRQR',
-                        'password' => 'uHGW5lebYSskH/kc8UJwokk/en4bXlnoSmGSjiFs+v8FLoSQVl8m+CYi2C0Y9b3kUpm/RjYA9jhYWpX6YCs1eQ==', //789
+                        'digest' => 'gNZ59vt+Vb2c4M6xUsZVDyMhPMN24IeEVNB5OG1OQdE/S3AMzrACY9ND+f1nThIJub2MLA2g5/XxblWluD4u2A==', //base64(SHA512(username + salt + 789))
                         'active' => '0',
                         'role' => '2')
                     );
@@ -145,8 +145,8 @@ Because my father offers us to take the private jet !'),
         }
         
         foreach ($users as $u) {
-            $file_db->exec("INSERT INTO users (username, salt, password, active, role) 
-                    VALUES ('{$u['username']}', '{$u['salt']}', '{$u['password']}','{$u['active']}', '{$u['role']}');");
+            $file_db->exec("INSERT INTO users (username, salt, digest, active, role) 
+                    VALUES ('{$u['username']}', '{$u['salt']}', '{$u['digest']}','{$u['active']}', '{$u['role']}');");
         }
         
         foreach ($mails as $m) {
@@ -182,7 +182,7 @@ Because my father offers us to take the private jet !'),
         foreach($resultsUsers as $row) {
             echo "Username: " . $row['username']."<br/>";
             echo "Salt: " . $row['salt'] . "<br/>";
-            echo "Passwor: " . $row['password'] . "<br/>";
+            echo "Digest: " . $row['digest'] . "<br/>";
             echo "Active: " . $row['active'] . "<br/>";
             echo "Role: " . $row['role'] . "<br/>";
             echo "<br/>";
